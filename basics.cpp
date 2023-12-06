94f15601cf2e35352b56fe4bd7378e56e2f21b65
Lisaag <lisatgroen@hotmail.com>
2023-12-06 14:18:35 +0100

    tri intersection check image generation

--- basics.cpp
+++ basics.cpp
@@ -1,5 +1,6 @@
 #include "precomp.h"
 #include "basics.h"
+#include "stb_image_write.h"
 
 // THIS SOURCE FILE:
 // Code for the article "How to Build a BVH", part 1: basics. Link:
@@ -16,8 +17,8 @@ TheApp* CreateApp() { return new BasicBVHApp(); }
 #define N	64
 
 // forward declarations
-void Subdivide( uint nodeIdx );
-void UpdateNodeBounds( uint nodeIdx );
+void Subdivide(uint nodeIdx);
+void UpdateNodeBounds(uint nodeIdx);
 
 // minimal structs
 struct Tri { float3 vertex0, vertex1, vertex2; float3 centroid; };
@@ -35,50 +36,54 @@ uint triIdx[N];
 BVHNode bvhNode[N * 2];
 uint rootNodeIdx = 0, nodesUsed = 1;
 
+//analyzation data
+uint8_t intrs_data[SCRHEIGHT * SCRWIDTH * 3]; //intersection count data for image generation
+int tri_intrs_count = 0; //number of times per ray a triangle intersection check was done
 // functions
 
-void IntersectTri( Ray& ray, const Tri& tri )
+void IntersectTri(Ray& ray, const Tri& tri)
 {
+	tri_intrs_count++;
 	const float3 edge1 = tri.vertex1 - tri.vertex0;
 	const float3 edge2 = tri.vertex2 - tri.vertex0;
-	const float3 h = cross( ray.D, edge2 );
-	const float a = dot( edge1, h );
+	const float3 h = cross(ray.D, edge2);
+	const float a = dot(edge1, h);
 	if (a > -0.0001f && a < 0.0001f) return; // ray parallel to triangle
 	const float f = 1 / a;
 	const float3 s = ray.O - tri.vertex0;
-	const float u = f * dot( s, h );
+	const float u = f * dot(s, h);
 	if (u < 0 || u > 1) return;
-	const float3 q = cross( s, edge1 );
-	const float v = f * dot( ray.D, q );
+	const float3 q = cross(s, edge1);
+	const float v = f * dot(ray.D, q);
 	if (v < 0 || u + v > 1) return;
-	const float t = f * dot( edge2, q );
-	if (t > 0.0001f) ray.t = min( ray.t, t );
+	const float t = f * dot(edge2, q);
+	if (t > 0.0001f) ray.t = min(ray.t, t);
 }
 
-bool IntersectAABB( const Ray& ray, const float3 bmin, const float3 bmax )
+bool IntersectAABB(const Ray& ray, const float3 bmin, const float3 bmax)
 {
 	float tx1 = (bmin.x - ray.O.x) / ray.D.x, tx2 = (bmax.x - ray.O.x) / ray.D.x;
-	float tmin = min( tx1, tx2 ), tmax = max( tx1, tx2 );
+	float tmin = min(tx1, tx2), tmax = max(tx1, tx2);
 	float ty1 = (bmin.y - ray.O.y) / ray.D.y, ty2 = (bmax.y - ray.O.y) / ray.D.y;
-	tmin = max( tmin, min( ty1, ty2 ) ), tmax = min( tmax, max( ty1, ty2 ) );
+	tmin = max(tmin, min(ty1, ty2)), tmax = min(tmax, max(ty1, ty2));
 	float tz1 = (bmin.z - ray.O.z) / ray.D.z, tz2 = (bmax.z - ray.O.z) / ray.D.z;
-	tmin = max( tmin, min( tz1, tz2 ) ), tmax = min( tmax, max( tz1, tz2 ) );
+	tmin = max(tmin, min(tz1, tz2)), tmax = min(tmax, max(tz1, tz2));
 	return tmax >= tmin && tmin < ray.t && tmax > 0;
 }
 
-void IntersectBVH( Ray& ray, const uint nodeIdx )
+void IntersectBVH(Ray& ray, const uint nodeIdx)
 {
 	BVHNode& node = bvhNode[nodeIdx];
-	if (!IntersectAABB( ray, node.aabbMin, node.aabbMax )) return;
+	if (!IntersectAABB(ray, node.aabbMin, node.aabbMax)) return;
 	if (node.isLeaf())
 	{
-		for (uint i = 0; i < node.triCount; i++ )
-			IntersectTri( ray, tri[triIdx[node.leftFirst + i]] );
+		for (uint i = 0; i < node.triCount; i++)
+			IntersectTri(ray, tri[triIdx[node.leftFirst + i]]);
 	}
 	else
 	{
-		IntersectBVH( ray, node.leftFirst );
-		IntersectBVH( ray, node.leftFirst + 1 );
+		IntersectBVH(ray, node.leftFirst);
+		IntersectBVH(ray, node.leftFirst + 1);
 	}
 }
 
@@ -92,30 +97,30 @@ void BuildBVH()
 	// assign all triangles to root node
 	BVHNode& root = bvhNode[rootNodeIdx];
 	root.leftFirst = 0, root.triCount = N;
-	UpdateNodeBounds( rootNodeIdx );
+	UpdateNodeBounds(rootNodeIdx);
 	// subdivide recursively
-	Subdivide( rootNodeIdx );
+	Subdivide(rootNodeIdx);
 }
 
-void UpdateNodeBounds( uint nodeIdx )
+void UpdateNodeBounds(uint nodeIdx)
 {
 	BVHNode& node = bvhNode[nodeIdx];
-	node.aabbMin = float3( 1e30f );
-	node.aabbMax = float3( -1e30f );
+	node.aabbMin = float3(1e30f);
+	node.aabbMax = float3(-1e30f);
 	for (uint first = node.leftFirst, i = 0; i < node.triCount; i++)
 	{
 		uint leafTriIdx = triIdx[first + i];
 		Tri& leafTri = tri[leafTriIdx];
-		node.aabbMin = fminf( node.aabbMin, leafTri.vertex0 ),
-		node.aabbMin = fminf( node.aabbMin, leafTri.vertex1 ),
-		node.aabbMin = fminf( node.aabbMin, leafTri.vertex2 ),
-		node.aabbMax = fmaxf( node.aabbMax, leafTri.vertex0 ),
-		node.aabbMax = fmaxf( node.aabbMax, leafTri.vertex1 ),
-		node.aabbMax = fmaxf( node.aabbMax, leafTri.vertex2 );
+		node.aabbMin = fminf(node.aabbMin, leafTri.vertex0),
+			node.aabbMin = fminf(node.aabbMin, leafTri.vertex1),
+			node.aabbMin = fminf(node.aabbMin, leafTri.vertex2),
+			node.aabbMax = fmaxf(node.aabbMax, leafTri.vertex0),
+			node.aabbMax = fmaxf(node.aabbMax, leafTri.vertex1),
+			node.aabbMax = fmaxf(node.aabbMax, leafTri.vertex2);
 	}
 }
 
-void Subdivide( uint nodeIdx )
+void Subdivide(uint nodeIdx)
 {
 	// terminate recursion
 	BVHNode& node = bvhNode[nodeIdx];
@@ -134,7 +139,7 @@ void Subdivide( uint nodeIdx )
 		if (tri[triIdx[i]].centroid[axis] < splitPos)
 			i++;
 		else
-			swap( triIdx[i], triIdx[j--] );
+			swap(triIdx[i], triIdx[j--]);
 	}
 	// abort split if one of the sides is empty
 	int leftCount = i - node.leftFirst;
@@ -148,11 +153,11 @@ void Subdivide( uint nodeIdx )
 	bvhNode[rightChildIdx].triCount = node.triCount - leftCount;
 	node.leftFirst = leftChildIdx;
 	node.triCount = 0;
-	UpdateNodeBounds( leftChildIdx );
-	UpdateNodeBounds( rightChildIdx );
+	UpdateNodeBounds(leftChildIdx);
+	UpdateNodeBounds(rightChildIdx);
 	// recurse
-	Subdivide( leftChildIdx );
-	Subdivide( rightChildIdx );
+	Subdivide(leftChildIdx);
+	Subdivide(rightChildIdx);
 }
 
 void BasicBVHApp::Init()
@@ -160,42 +165,48 @@ void BasicBVHApp::Init()
 	// intialize a scene with N random triangles
 	for (int i = 0; i < N; i++)
 	{
-		float3 r0 = float3( RandomFloat(), RandomFloat(), RandomFloat() );
-		float3 r1 = float3( RandomFloat(), RandomFloat(), RandomFloat() );
-		float3 r2 = float3( RandomFloat(), RandomFloat(), RandomFloat() );
-		tri[i].vertex0 = r0 * 9 - float3( 5 );
+		float3 r0 = float3(RandomFloat(), RandomFloat(), RandomFloat());
+		float3 r1 = float3(RandomFloat(), RandomFloat(), RandomFloat());
+		float3 r2 = float3(RandomFloat(), RandomFloat(), RandomFloat());
+		tri[i].vertex0 = r0 * 9 - float3(5);
 		tri[i].vertex1 = tri[i].vertex0 + r1, tri[i].vertex2 = tri[i].vertex0 + r2;
 	}
 	// construct the BVH
 	BuildBVH();
 }
 
-void BasicBVHApp::Tick( float deltaTime )
+void BasicBVHApp::Tick(float deltaTime)
 {
 	// draw the scene
-	screen->Clear( 0 );
+	screen->Clear(0);
 	// define the corners of the screen in worldspace
-	float3 p0( -1, 1, -15 ), p1( 1, 1, -15 ), p2( -1, -1, -15 );
+	float3 p0(-1, 1, -15), p1(1, 1, -15), p2(-1, -1, -15);
 	Ray ray;
 	Timer t;
-	for (int y = 0; y < SCRHEIGHT; y++) for (int x = 0; x < SCRWIDTH; x++)
-	{
+
+	int ind = 0;
+	for (int y = 0; y < SCRHEIGHT; y++) for (int x = 0; x < SCRWIDTH; x++) {
 		// calculate the position of a pixel on the screen in worldspace
 		float3 pixelPos = p0 + (p1 - p0) * (x / (float)SCRWIDTH) + (p2 - p0) * (y / (float)SCRHEIGHT);
 		// define the ray in worldspace
-		ray.O = float3( 0, 0, -18 );
-		ray.D = normalize( pixelPos - ray.O );
+		ray.O = float3(0, 0, -18);
+		ray.D = normalize(pixelPos - ray.O);
 		// initially the ray has an 'infinite length'
 		ray.t = 1e30f;
-	#if 0
-		for( int i = 0; i < N; i++ ) IntersectTri( ray, tri[i] );
-	#else
-		IntersectBVH( ray, rootNodeIdx );
-	#endif
-		if (ray.t < 1e30f) screen->Plot( x, y, 0xffffff );
+#if 0
+		for (int i = 0; i < N; i++) IntersectTri(ray, tri[i]);
+#else
+		IntersectBVH(ray, rootNodeIdx);
+#endif
+		if (ray.t < 1e30f) screen->Plot(x, y, 0xffffff);
+
+		intrs_data[ind++] = 0, intrs_data[ind++] = (int)((255.0f / (N)) * tri_intrs_count), intrs_data[ind++] = 0;
+		tri_intrs_count = 0;
 	}
 	float elapsed = t.elapsed() * 1000;
-	printf( "tracing time: %.2fms (%5.2fK rays/s)\n", elapsed, sqr( 630 ) / elapsed );
+	printf("tracing time: %.2fms (%5.2fK rays/s)\n", elapsed, sqr(630) / elapsed);
+
+	stbi_write_png("intsection-count.png", SCRWIDTH, SCRHEIGHT, 3, intrs_data, (int)(sizeof(uint8_t) * SCRWIDTH * 3));
 }
 
 // EOF
